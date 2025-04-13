766c5763c2db081deb8641661aa8762525437dea
Vishnu V <vishnuvirat564@gmail.com>
2025-04-13 12:08:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,15 +4,29 @@ class EventTarget {
     }
   
     addEventListener(event, callback) {
-      
+           if(!this.listeners.has(event)){
+			   this.listeners.set(event, new Set());
+		   }
+		   this.listeners.get(event).add(callback);
     }
   
     removeEventListener(event, callback) {
-      
+	      if(this.listeners.has(event)){
+			  const callbacks=this.listeners.get(event);
+			  callbacks.delete(callback);
+			  if(callbacks.size===0){
+				  this.listeners.delete(event);
+			  }
+		  }
     }
   
     dispatchEvent(event) {
-    
+          if(this.listeners.has(event)){
+			   const set=this.listeners.get(event);
+			  for(const values of set.values()){
+				   values();
+			  }
+		  }
     }
     
   }
